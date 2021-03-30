9e46eef0aa20da8229026f5f1965dfb7d9735731
Gabriel Leao Vendramini <gabriel_vendramini@hotmail.com>
2021-03-30 14:42:59 +0200

    3 commit

--- app/src/main/java/com/alura/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -13,16 +13,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import static com.alura.agenda.R.layout.activity_lista_alunos;
+
+public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(activity_lista_alunos);
 
         List<String> alunos = new ArrayList<>(Arrays.asList("Pedro", "Ana", "Julia", "Joana", "Carlos", "Olivia"));
 
-       ListView llistaAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+       ListView llistaAlunos = findViewById(R.id.activity_lista_alunos_listView);
        llistaAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
 
 
