90c5f0dff2213286e662c474b408f45c1e30c9a1
Gabriel Leao Vendramini <gabriel_vendramini@hotmail.com>
2021-03-30 14:19:52 +0200

    1 commit

---
+++ app/src/main/java/com/alura/agenda/MainActivity.java
@@ -0,0 +1,28 @@
+package com.alura.agenda;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<String> alunos = new ArrayList<>(Arrays.asList("Pedro", "Ana", "Julia", "Joana", "Carlos", "Olivia"));
+
+       ListView llistaAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+       llistaAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
+
+
+    }
+}
